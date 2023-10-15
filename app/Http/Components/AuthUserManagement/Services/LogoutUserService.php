f4e3d2df076d7c8ea8cf789fb23fdac9633a7a41
Dennis Disselhoff <Dennismaxdisselhoff@gmail.com>
2023-10-15 19:52:39 +0200

    feat: add logout

--- app/Http/Components/AuthUserManagement/Services/LogoutUserService.php
+++ app/Http/Components/AuthUserManagement/Services/LogoutUserService.php
@@ -3,13 +3,13 @@
 namespace App\Http\Components\AuthUserManagement\Services;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LogoutUserService
 {
     public function logoutUser(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        $request->session()->invalidate();
+
 
         return [
             'message' => __(
